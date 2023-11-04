24c1a585e1e3efcab62a6802d380d10f7e79641c
CosminVulpe <cosmin98vulpe@gmail.com>
2023-11-04 19:22:59 +0400

    Delete unused annotation

--- src/main/java/com/jdm/legends/users/UsersApplication.java
+++ src/main/java/com/jdm/legends/users/UsersApplication.java
@@ -2,11 +2,9 @@ package com.jdm.legends.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EntityScan(basePackages = {"com.jdm.legends.common", "com.jdm.legends.users"})
 @SpringBootApplication
 public class UsersApplication {
 
