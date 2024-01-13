748c87365690f16da2195d13704921d692a419fd
CosminVulpe <cosmin98vulpe@gmail.com>
2024-01-13 17:53:22 +0400

    Add max age CORS

--- src/main/java/com/jdm/legends/customers/cors/ConfigGlobalCors.java
+++ src/main/java/com/jdm/legends/customers/cors/ConfigGlobalCors.java
@@ -1,17 +1,24 @@
 package com.jdm.legends.customers.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ConfigGlobalCors implements WebMvcConfigurer {
+    private final int maxAge;
+
+    public ConfigGlobalCors(@Value("${cors.max-age-time}") int maxAge) {
+        this.maxAge = maxAge;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("http://localhost:3000/")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(maxAge);
     }
 }
