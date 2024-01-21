1b9d08d19e6f86bf85aa858b41fd55f657ebd276
CosminVulpe <cosmin98vulpe@gmail.com>
2024-01-21 17:51:13 +0400

    fix tests

--- src/test/java/com/jdm/legends/customers/integration/controller/ReminderEmailControllerIT.java
+++ src/test/java/com/jdm/legends/customers/integration/controller/ReminderEmailControllerIT.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @ActiveProfiles("test-in-memory")
 @Sql("/add-temporary-customers.sql")
+@WithMockUser(username = "john.doe", authorities = {"ROLE_CLIENT"})
 public class ReminderEmailControllerIT {
     @Autowired
     private MockMvc mockMvc;
